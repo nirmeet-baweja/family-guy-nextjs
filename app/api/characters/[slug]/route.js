d0beb483e1be73ab06c01657702557dffd091898
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-27 23:45:26 +0100

    dynamic get api route for single character

---
+++ app/api/characters/[slug]/route.js
@@ -0,0 +1,32 @@
+/**
+ * Retrieves a character and their associated quotes based on the provided slug.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} params - The route parameters.
+ * @param {string} params.slug - The slug of the character.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the character and their quotes, or an error response.
+ */
+
+import characters from "@/data/characters";
+import quotes from "@/data/quotes";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  try {
+    const character = characters.data.find((item) => item.slug === params.slug);
+    if (!character) {
+      return new NextResponse("not found", { status: 404 });
+    }
+    const characterQuotes = quotes.data.filter(
+      (item) => item.character_id === character.id
+    );
+
+    return NextResponse.json({
+      character,
+      character_quotes: characterQuotes.length > 0 ? characterQuotes : null,
+    });
+  } catch (error) {
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+};
