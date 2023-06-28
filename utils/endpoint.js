731067a90ceb242181efff3b85fa3b8c32af2359
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-28 11:10:36 +0100

    try to fix api deployment issue

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,8 @@
+import path from "path";
+
 const dev = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production";
-export const endpoint =
-  (dev ? `http://` : `https://`) + `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`;
+export const endpoint = path.join(
+  dev ? `http:/` : `https:/`,
+  process.env.NEXT_PUBLIC_VERCEL_URL,
+  "api"
+);
