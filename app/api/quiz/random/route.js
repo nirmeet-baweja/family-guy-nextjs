8cc812bff46ff6539d2efe67ed6eb79ac898593a
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-07-04 16:09:09 +0100

    add quiz page

---
+++ app/api/quiz/random/route.js
@@ -0,0 +1,13 @@
+import questions from "@/data/quiz.json";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    const randomIndex = Math.floor(Math.random() * questions.data.length);
+    return new NextResponse.json({
+      randomQuestion: questions.data[randomIndex].id,
+    });
+  } catch (error) {
+    return new NextResponse("Internal Server Error.", { status: 500 });
+  }
+};
